58d9a6984964d72731d8585b20a9c6ebc27d46a8
Saimanaswini22 <saimanaswinibalguri2207@gmail.com>
2024-09-09 16:52:16 +0530

    pipeline

---
+++ simpleWebApp/src/main/java/com/example/simpleWebApp/service/ProductService.java
@@ -0,0 +1,44 @@
+package com.example.simpleWebApp.service;
+
+import com.example.simpleWebApp.model.Product;
+import com.example.simpleWebApp.repository.ProductRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    ProductRepo repo;
+
+//    List<Product> products= new ArrayList<>(Arrays.asList(new Product(101,"Oneplus",30000),
+//            new Product(102,"ZBook",60000),
+//            new Product(103,"Airdopes",4000)));
+
+    public List<Product> getProducts(){
+
+        return repo.findAll();
+    }
+
+    public Product getProductById(int prodId) {
+        return repo.findById(prodId).orElse(new Product());
+    }
+
+    public void addProduct(Product prod){
+
+        repo.save(prod);
+    }
+
+
+    public void updateProduct(Product prod) {
+        repo.save(prod);
+    }
+
+    public void deleteProduct(int prodId) {
+        repo.deleteById(prodId);
+    }
+}
